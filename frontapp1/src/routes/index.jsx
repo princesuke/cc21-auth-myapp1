427ea517742d1b9e28a25bc57a8aa891e5ae31b9
prince <princesuke@gmail.com>
2025-10-17 15:01:14 +0700

    feat: add frontend

---
+++ frontapp1/src/routes/index.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>home1</div>,
+  },
+]);
+
+export default router;
